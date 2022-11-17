9bf910df664edf87ba33b22d60223b8a208bc2f1
EC2 Default User <fumipro0913@gmail.com>
2022-11-17 12:51:19 +0000

    変数

---
+++ lesson2/calcurate.js
@@ -0,0 +1,8 @@
+let str1 = 10;
+let str2 = 3;
+alert(str1 + str2);
+alert(str1 - str2);
+alert(str1 * str2);
+alert(str1 / str2);
+
+// 4回連続でアラート分が出る
---
+++ lesson2/for.js
@@ -0,0 +1,7 @@
+let i;
+let num = 0;
+for(i = 0; i <= 10;) {
+  num = num + i;
+  i = i + 1;
+}
+alert("1から10まで足し算を行なった結果は" + num + "です。")
